c55f3f9475030bbe5df88875ed5f583514697428
tarik-hamedovic <tarik.hamedovic@gmail.com>
2025-06-12 07:28:36 +0200

    Update

--- 2.soc-litex/1.hw/adc/adc.v
+++ 2.soc-litex/1.hw/adc/adc.v
@@ -1,136 +1,87 @@
-//////////////////////////////////////////////////////////////////////////////////
-//                                                                              //
-//                                                                              //
-//  Author: meisq                                                               //
-//          msq@qq.com                                                          //
-//          ALINX(shanghai) Technology Co.,Ltd                                  //
-//          heijin                                                              //
-//     WEB: http://www.alinx.cn/                                                //
-//     BBS: http://www.heijin.org/                                              //
-//                                                                              //
-//////////////////////////////////////////////////////////////////////////////////
-//                                                                              //
-// Copyright (c) 2017,ALINX(shanghai) Technology Co.,Ltd                        //
-//                    All rights reserved                                       //
-//                                                                              //
-// This source file may be used and distributed without restriction provided    //
-// that this copyright statement is not removed from the file and that any      //
-// derivative work contains the original copyright notice and the associated    //
-// disclaimer.                                                                  //
-//                                                                              //
-//////////////////////////////////////////////////////////////////////////////////
-
-//================================================================================
-//  Revision History:
-//  Date          By            Revision    Change Description
-//--------------------------------------------------------------------------------
-//2017/8/28                   1.0          Original
-//*******************************************************************************/
 module adc(
-	input                       adc_clk,
-	input                       rst,
-	input[11:0]                 adc_data,
-	output reg                  adc_buf_wr,
-	output[11:0]                adc_buf_addr,
-	output[7:0]                 adc_buf_data
+   //sys
+   input                            sys_clk,               //system clock negative
+   input                            rst_n,                   //reset ,low active
+
+   //ADC
+   output                           adc_clk_ch0,          //AD channel 0 sampling clock
+   output                           adc_clk_ch1,          //AD channel 1 sampling clock
+   input  [11:0]                    adc_data_ch0,         //AD channel 0 data
+   input  [11:0]                    adc_data_ch1,         //AD channel 1 data
+   output [11:0]                    ad_data_ch0,
+   output [11:0]                    ad_data_ch1
 );
 
-//`define TRIGGER
 
-localparam       S_IDLE    = 0;
-localparam       S_SAMPLE  = 1;
-localparam       S_WAIT    = 2;
+    wire [11:0] adc0_rising,  adc0_falling;
+    wire [11:0] adc1_rising,  adc1_falling;
 
-reg signed[11:0]  adc_data_d0;
-reg signed[12:0] adc_data_offset;
-reg[7:0] adc_data_narrow;
-reg[7:0] adc_data_narrow_d0;
-reg[10:0] sample_cnt;
-reg[31:0] wait_cnt;
-reg[2:0] state;
-assign adc_buf_addr = sample_cnt;
-assign adc_buf_data = adc_data_narrow;
-always@(posedge adc_clk or posedge rst)
-begin
-	if(rst == 1'b1)
-		adc_data_d0 <= 12'd0;
-	else
-		adc_data_d0 <= {~adc_data[11],adc_data[10:0]};
-end
+    genvar i;
+    generate
+      for (i = 0; i < 12; i = i + 1) begin : gen_iddr_ch0
+        IDDR #(
+          .DDR_CLK_EDGE    ("SAME_EDGE_PIPELINED"), // Q1 and Q2 valid on rising edge
+          .INIT_Q1         (1'b0),
+          .INIT_Q2         (1'b0)
+        ) iddr_ch0_i (
+          .Q1  (adc0_rising[i]),       // data from rising edge
+          .Q2  (adc0_falling[i]),      // data from falling edge
+          .C   (sys_clk),      // ADC clk
+          .CE  (1'b1),
+          .D   (adc_data_ch0[i]),  // DDR input pin
+          .R   (1'b0),
+          .S   (1'b0)
+        );
+      end
+      for (i = 0; i < 12; i = i + 1) begin : gen_iddr_ch1
+        IDDR #(
+          .DDR_CLK_EDGE    ("SAME_EDGE_PIPELINED"),
+          .INIT_Q1         (1'b0),
+          .INIT_Q2         (1'b0)
+        ) iddr_ch1_i (
+          .Q1  (adc1_rising[i]),
+          .Q2  (adc1_falling[i]),
+          .C   (sys_clk),
+          .CE  (1'b1),
+          .D   (adc_data_ch1[i]),
+          .R   (1'b0),
+          .S   (1'b0)
+        );
+      end
+    endgenerate
 
-always@(posedge adc_clk or posedge rst)
-begin
-	if(rst == 1'b1)
-		adc_data_offset <= 13'd0;
-	else
-		adc_data_offset <= adc_data_d0 + 13'd2048;
-end
+//   assign adc_clk_ch0  = adc_clk;
+    ODDR #(
+    .DDR_CLK_EDGE           ("SAME_EDGE"           )
+    )
+    ODDR_clk_ch0
+    (
+    .Q                      (adc_clk_ch0          ),              // 1-bit DDR output data
+    .C                      (sys_clk           ),              // 1-bit clock input
+    .CE                     (1'b1                    ),              // 1-bit clock enable input
+    .D1                     (1'b1                    ),              // 1-bit data input (associated with C)
+    .D2                     (1'b0                    ),              // 1-bit data input (associated with C)
+    .R                      (1'b0                    ),              // 1-bit reset input
+    .S                      (1'b0                    )               // 1-bit set input
+    );
 
-always@(posedge adc_clk or posedge rst)
-begin
-	if(rst == 1'b1)
-		adc_data_narrow <= 8'd0;
-	else
-		adc_data_narrow <= adc_data_offset[11:4];
-end
+//   assign adc_clk_ch1  = adc_clk;
+    ODDR #(
+    .DDR_CLK_EDGE           ("SAME_EDGE"           )
+    )
+    ODDR_clk_ch1
+    (
+    .Q                      (adc_clk_ch1          ),              // 1-bit DDR output data
+    .C                      (sys_clk           ),              // 1-bit clock input
+    .CE                     (1'b1                    ),              // 1-bit clock enable input
+    .D1                     (1'b1                    ),              // 1-bit data input (associated with C)
+    .D2                     (1'b0                    ),              // 1-bit data input (associated with C)
+    .R                      (1'b0                    ),              // 1-bit reset input
+    .S                      (1'b0                    )               // 1-bit set input
+    );
 
-always@(posedge adc_clk or posedge rst)
-begin
-	if(rst == 1'b1)
-		adc_data_narrow_d0 <= 8'd0;
-	else
-		adc_data_narrow_d0 <= adc_data_narrow_d0;
-end
-always@(posedge adc_clk or posedge rst)
-begin
-	if(rst == 1'b1)
-	begin
-		state <= S_IDLE;
-		wait_cnt <= 32'd0;
-		sample_cnt <= 11'd0;
-		adc_buf_wr <= 1'b0;
-	end
-	else
-		case(state)
-			S_IDLE:
-			begin
-				state <= S_SAMPLE;
-			end
-			S_SAMPLE:
-			begin
-				if(sample_cnt == 11'd1023)
-				begin
-					sample_cnt <= 11'd0;
-					adc_buf_wr <= 1'b0;
-					state <= S_WAIT;
-				end
-				else
-				begin
-					sample_cnt <= sample_cnt + 11'd1;
-					adc_buf_wr <= 1'b1;
-				end
+assign ad_data_ch0 = adc0_rising;
+assign ad_data_ch1 = adc1_rising;
 
-			end
-			S_WAIT:
-			begin
-`ifdef  TRIGGER
-				if(adc_data_narrow_d0 < 8'd127 && adc_data_narrow >= 8'd127)
-					state <= S_SAMPLE;
-`else
-				if(wait_cnt == 32'd25_000_000)
-				begin
-					state <= S_SAMPLE;
-					wait_cnt <= 32'd0;
-				end
-				else
-				begin
-					wait_cnt <= wait_cnt + 32'd1;
-				end
-`endif
-			end
-			default:
-				state <= S_IDLE;
-		endcase
-end
 
 endmodule
