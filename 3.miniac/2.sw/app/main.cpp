9e5068ce1b5227bc5ec449db84b0f4bec5a1d184
Nikola Sokolović <nikola.sokolovic@chili-chips.xyz>
2025-06-23 19:11:22 +0200

    Update main.cpp
    Added reading from ADC and sending data to DAC via CSR

--- 3.miniac/2.sw/app/main.cpp
+++ 3.miniac/2.sw/app/main.cpp
@@ -16,14 +16,21 @@ int main(void)
    // Turn on LED2
    csr->gpio->led2(1);
 
-   // Set LED1 to the value of KEY1
-   csr->gpio->led1(csr->gpio->key1());
+   
 
    // Send Hello world to UART
    uart_send(csr, "Hello world!\r\n");
+   
+   while(1){
+        // Set LED1 to the value of KEY1
+        csr->gpio->led1(csr->gpio->key1());
+   
+        csr->dac->ch1(csr->adc->ch1() * 4);
+        csr->dac->ch2(csr->adc->ch2() * 4);
+   }
 
    // Receive (and echo) the text terminated with CRLF
-   while(!uart_recv(csr, rx_data));
+   /*while(!uart_recv(csr, rx_data));
 
    uart_send(csr, "VENDOR  = ");
    uart_send_hex(csr, csr->hw_id->VENDOR(), 4);
@@ -35,7 +42,7 @@ int main(void)
    uart_send_dec(csr, csr->hw_version->MINOR());
    uart_send_char(csr, '.');
    uart_send_dec(csr, csr->hw_version->PATCH());
-   uart_send(csr, "\r\n");
+   uart_send(csr, "\r\n");*/
 
    return 0;
 }
